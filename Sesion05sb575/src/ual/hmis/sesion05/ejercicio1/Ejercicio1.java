4f369a503da5953cc910382aa46adedd2af07591
SOKAINA BAOUCH <sb575@inlumine.ual.es>
2021-05-16 16:57:20 +0200

    Resolviendo algunos errores del análisis estático

--- Sesion05sb575/src/ual/hmis/sesion05/ejercicio1/Ejercicio1.java
+++ Sesion05sb575/src/ual/hmis/sesion05/ejercicio1/Ejercicio1.java
@@ -3,17 +3,18 @@ package ual.hmis.sesion05.ejercicio1;
 public class Ejercicio1 {
 
 
-	public int transformar(int x) {
+	public int transformar(int x){
 		
 		int resultado = 0;
-		if (x % 2 == 0) // % Resto de una divisi�n entre enteros (mod)
+		if (x % 2 == 0) {  //% Resto de una division entre enteros (mod)
 			resultado = transformar (x/2);
-		else if (x % 3 == 0)
+		}else if (x % 3 == 0){
 			resultado = transformar (x/3);
-		else if (x % 5 == 0)
+		}else if (x % 5 == 0) {
 			resultado = transformar (x/5);
-		else return x;
-		
+		}else {
+			return x;
+		}
 		return resultado;
 		
 	}
